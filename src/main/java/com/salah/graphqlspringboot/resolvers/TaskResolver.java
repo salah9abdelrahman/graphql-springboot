a7a73c22728bf5e1e561cc0a0ffde6e9a5768ccb
salah9abdelrahman <salah9abdelrahman@outlook.com>
2022-04-30 14:12:02 +0200

    function resolver happens synchronous by default so let's add an A

--- src/main/java/com/salah/graphqlspringboot/resolvers/TaskResolver.java
+++ src/main/java/com/salah/graphqlspringboot/resolvers/TaskResolver.java
@@ -1,22 +1,28 @@
 package com.salah.graphqlspringboot.resolvers;
 
 
-import com.salah.graphqlspringboot.exception.custom.GraphQLException;
+//import com.salah.graphqlspringboot.exception.custom.GraphQLException;
 import com.salah.graphqlspringboot.model.Task;
 import com.salah.graphqlspringboot.services.TaskService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Component
 public class TaskResolver  implements GraphQLQueryResolver {
     private final TaskService taskService;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public TaskResolver(TaskService taskService) {
         this.taskService = taskService;
     }
 
-    public Task getTask(Long id){
-        throw new RuntimeException("custom exception message yo!");
-//        return taskService.getTask(id);
+    public CompletableFuture<Task> getTask(Long id){
+//        throw new RuntimeException("custom exception message yo!");
+        return CompletableFuture.supplyAsync(() -> taskService.getTask(id), executorService);
     }
 }
